de5ade4a6d0ae4acd663f5761193310889b6f12d
jon <jrinciari@or-specialties.com>
2017-11-30 20:35:34 +0000

    Added index page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var newLink = require('../controllers/newLink.js')
 var redirect_controller = require('../controllers/redirect_controller')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Url Shortener' });
 });
 
 router.get("/new/:url(*)", newLink)
