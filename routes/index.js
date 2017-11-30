643694baf23839276002ac6088eaec771abdea98
jon <jrinciari@or-specialties.com>
2017-11-30 16:57:28 +0000

    added redirect for shorter url, will not access db and pull original link

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,50 @@ var express = require('express');
 var router = express.Router();
 var UrlModel = require('../models/url_shortener');
 var validUrl = require('valid-url');
-var urlShortener = require('../public/javascripts/urlshortener')
 var shortid = require('shortid');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/new/:url(*)", function(req, res){
+router.get("/new/:url(*)", function(req, res, next){
     var newLink = req.params.url
     if(validUrl.isUri(newLink)){
-       UrlModel.find({long_url: newLink}, function(err, docs){
-        if (err) throw err
-        if(docs.length){
-            res.json(docs) 
+       UrlModel.find({long_url: newLink}, function(err, link){
+        if (err){
+            res.json('Error: There has been an issue accessing the database')
+        }
+        if(link.length){
+            res.json(link) 
        } else {
             var id = shortid.generate();
-            var shorturl = `http://www.${req.hostname}/${id}`
+            var shorturl = `http://${req.hostname}/${id}`
             UrlModel.create({long_url: newLink, short_url: shorturl}, function(err, instance){
-                if(err) throw err;
-                else {
+                if(err) {
+                    res.json('Error: We could not process your request')
+                }else {
                     res.json(instance)
                 }
             })
        }
     })
     } else {
-        console.log('not valid link')
+        res.json('Error: This is not a valid link')
     }
 })
 router.get('/:ENCODED_URL', function(req, res, next){
-    res.send('Alternative Link')
+    UrlModel.findOne({
+        short_url: `http://${req.hostname}/${req.params.ENCODED_URL}`
+    },function(err, found){
+        if(err){
+            res.json('Error: We could not redirect you to the original link')
+        }
+        if(found){
+           res.redirect(found.long_url)
+        }
+    })
+        
+ 
 })
 
-    //   var obj = urlShortener(newLink, req.hostname)
 module.exports = router;
